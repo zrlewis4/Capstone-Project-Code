2158e0afaa0650e3b32520ee05524964bc5188f9
zlewis <zlewisman1999@gmail.com>
2021-11-09 13:06:20 -0600

    Changed filename for testing

--- tests/image_avg.py
+++ tests/image_avg.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-video = cv2.VideoCapture(r"C:\Users\Zachary Lewis\Documents\Bradley\Current\Capstone Project\Capstone Project Code\data\blue.mp4")
+video = cv2.VideoCapture(r"C:\Users\Zachary Lewis\Documents\Bradley\Current\Capstone Project\Capstone Project Code\data\test_intensity.mp4")
 value = []
 
 while (True):
@@ -12,9 +12,13 @@ while (True):
 
     if ret:
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("Live", image)
         value.append(np.mean(frame.astype(float)))
     else:
         break
+    #key = cv2.waitKey(1)
+    #if key == ord("q"):
+    #    break
     
 
 video.release()
